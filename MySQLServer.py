1d74b09948c931f8fcab7f0ce766374d4f65691f
Farid Seidu <Versedcamel153@gmail.com>
2024-07-27 16:04:24 +0000

    Added DB files

---
+++ MySQLServer.py
@@ -0,0 +1,33 @@
+import mysql.connector
+from mysql.connector import Error
+
+def create_database():
+    try:
+        connection = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='far_id1'
+        )
+
+        if connection.is_connected():
+            cursor = connection.cursor()
+
+            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_bookstore;")
+
+            print("Database 'alx_bookstore' created successfully!")
+        
+    except Error as e:
+        print(f"Error while connecting to mySQL: {e}")
+
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+
+if __name__ == '__main__':
+    create_database()
+
+
+
+
---
+++ alx_book_store.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE IF NOT EXISTS
+
+USE alx_bookstore;
+
+CREATE TABLE Authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    suthor_name VARCHAR(215) NOT NULL
+);
+
+CREATE TABLE Books (
+    book_id PRIMARY KEY  INT AUTO_INCREMENT,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    price DOUBLE NOT NULL,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+)
+
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL,
+    address TEXT NOT NULL,
+)
+
+CREATE TABLE Orders (
+    order_id PRIMARY KEY INT AUTO_INCREMENT,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+)
+
+CREATE TABLE Order_Details (
+    order_detail PRIMARY KEY INT AUTO_INCREMENT,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE NOT NULL,
+    FOREIGN KEY(order_id) REFERENCES Orders(order_id)
+    FOREIGN KEY(book_id) REFERENCES Books(book_id)
+)
